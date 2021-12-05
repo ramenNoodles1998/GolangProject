1d9e8830d50f03e1bfce0ef7481c52e2f7194eda
Roman Meredith <10769789@uvu.edu>
2021-12-05 11:58:17 -0700

    added more routes, and experimented with a put route

--- tutorial.go
+++ tutorial.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-    "github.com/gorilla/mux"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -31,9 +33,42 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: singleArticle")
 	vars:= mux.Vars(r)
 	key := vars["id"]
+
 	fmt.Println(Articles[key])
 	json.NewEncoder(w).Encode(Articles[key])
+}
 
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+
+	Articles[article.Id] = article
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+
+	delete(Articles, id)
+
+	fmt.Fprintf(w, "deleted article")
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+
+	json.Unmarshal(reqBody, &article)
+
+	Articles[id] = article 
+
+	json.NewEncoder(w).Encode(article)
 }
 
 func handleRequests() {
@@ -42,16 +77,22 @@ func handleRequests() {
 
 	myRouter.HandleFunc("/", homepage)
 	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
 	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
 	fmt.Println("rest api v2.0 mux routers")
+	fmt.Println("runnint on port :10000")
+
 	Articles = map[string]Article {
 		"1": {Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
 		"2": {Id: "2", Title: "Hello 2", Desc: "Article Description 2", Content: "Article Content 2"},
-	} 	
+	}
+
 	handleRequests()
 }
